9cfed5f7ba00a286d445b61f2bc76599f0e20ede
lsy <lsy@Suyounui-MacBook-Pro.local>
2017-07-25 18:22:06 +0900

    bookList 신규생성

--- src/main/java/model/BookVO.java
+++ src/main/java/model/BookVO.java
@@ -1,7 +1,113 @@
 package model;
 
+import java.util.Date;
+
 public class BookVO {
 
+    private String title;
+    private String genre;
+    private String writer;
+    private String translator;
+    private String publisher;
+    private String price;
+    private String isbn;
+    private String page;
+    private Date createDate;
+    private Date updateDate;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTranslator() {
+        return translator;
+    }
+
+    public void setTranslator(String translator) {
+        this.translator = translator;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BookVO{" +
+                "title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", writer='" + writer + '\'' +
+                ", translator='" + translator + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", price='" + price + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", page='" + page + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                '}';
+    }
 }
